bdcebc12d59cbe9d2562620444807b5ffb48e96a
Henner Kollmann <henner.kollmann@gmx.de>
2020-06-03 22:37:42 +0200

    add first version

---
+++ compile.js
@@ -0,0 +1,141 @@
+const path = require("path")
+const child_process = require("child_process");
+
+qx.Class.define("qxl.testnode.LibraryApi", {
+  extend: qx.tool.cli.api.LibraryApi,
+  members: {
+
+    async initialize() {
+      let yargs = qx.tool.cli.commands.Test.getYargsCommand;
+      qx.tool.cli.commands.Test.getYargsCommand = () => {
+        let args = yargs();
+        args.builder.class = {
+          describe: "only run tests of this class",
+          type: "string"
+        };
+        args.builder.method = {
+          describe: "only run tests of this method",
+          type: "string"
+        };
+        args.builder.diag = {
+          describe: "show diagnostic output",
+          type: "boolean"
+        };
+        args.builder.terse = {
+          describe: "show only summary and errors",
+          type: "boolean"
+        };
+        return args;
+      }
+    },
+
+    load: async function () {
+      let command = this.getCompilerApi().getCommand();
+      if (command instanceof qx.tool.cli.commands.Test) {
+        command.addListener("runTests", this.__onRunTests, this);
+      }
+    },
+
+
+    __onRunTests: function (data) {
+      let result = data.getData();
+
+      let app = this.getTestApp("qxl.testnode.Application");
+      if (!app) {
+        qx.tool.compiler.Console.print("Please install qxl.testnode package!");
+        return qx.Promise.resolve(false);
+      }
+      let target = app.maker.getTarget();
+      let outputDir = target.getOutputDir();
+      let boot = path.join(outputDir, app.name, "boot.js");
+      let args = [];
+      args.push(boot);
+      if (app.argv.method) {
+        args.push(` method=${app.argv.method}`);
+      }
+      if (app.argv.class) {
+        args.push(` class=${app.argv.class}`);
+      }
+      return new qx.Promise((resolve, reject) => {
+        let notOk = 0;
+        let Ok = 0;
+        let proc = child_process.spawn('node', args, {
+          cwd: '.',
+          shell: true
+        });
+        proc.stdout.on('data', (data) => {
+          let arr = data.toString().split("\n");
+          // value is serializable
+          arr.forEach(val => {
+            if (val.match(/^\d+\.\.\d+$/)) {
+              console.log(`DONE testing ${Ok} ok, ${notOk} not ok`);
+              result.errorCode += notOk;
+            } else if (val.match(/^not ok /)) {
+              notOk++;
+              qx.tool.compiler.Console.log(val);
+            } else if (val.match(/^ok\s/)) {
+              Ok++;
+              if (!app.argv.terse) {
+                console.log(val);
+              }
+            } else if (val.match(/^#/) && app.argv.diag) {
+              console.log(val);
+            } else if (app.argv.verbose) {
+              console.log(val);
+            }
+          });
+        });
+        proc.stderr.on('data', (data) => {
+          let val = data.toString();
+          console.error(val); s
+        });
+        proc.on('close', code => {
+          resolve(code);
+        });
+        proc.on('error', () => {
+          reject()
+        });
+      });
+    },
+
+
+    getTestApp: function (classname) {
+      let command = this.getCompilerApi().getCommand();
+      let maker = null;
+      let app = null;
+      command.getMakers().forEach(tmp => {
+        let apps = tmp.getApplications().filter(app => (app.getClassName() === classname) && !app.isBrowserApp());
+        if (apps.length) {
+          if (maker) {
+            qx.tool.compiler.Console.print("Found to many makers for app");
+            return null;
+          }
+          if (apps.length != 1) {
+            qx.tool.compiler.Console.print(`found to many apps for classname ${classname}`);
+            return null;
+          }
+          maker = tmp;
+          app = apps[0];
+        }
+      });
+      if (!app) {
+        qx.tool.compiler.Console.print(`no apps found for classname ${classname}`);
+        return null;
+      }
+      return {
+        name: app.getName(),
+        argv: command.argv,
+        environment: app.getEnvironment(),
+        maker: maker
+      }
+    },
+    _cnt: null,
+    _failed: null
+  }
+
+
+});
+
+module.exports = {
+  LibraryApi: qxl.testnode.LibraryApi
+};
---
+++ source/class/qxl/testnode/Application.js
@@ -0,0 +1,145 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+/**
+ * This is the main application class of your custom application "qxl.testnode".
+ *
+ * If you have added resources to your app, remove the first '@' in the
+ * following line to make use of them.
+ * @@asset(qxl/testnode/*)
+ *
+ */
+qx.Class.define("qxl.testnode.Application",
+  {
+    extend: qx.application.Basic,
+    members:
+    {
+      main: async function () {
+        if (qx.core.Environment.get("runtime.name") == "rhino") {
+          qx.log.Logger.register(qx.log.appender.RhinoConsole);
+        } else if (qx.core.Environment.get("runtime.name") == "node.js") {
+          qx.log.Logger.register(qx.log.appender.NodeConsole);
+        }
+        await this.runTest();
+        return this._fail;
+      },
+
+      runTest: async function () {
+        this._cnt = 0;
+        this._fail = 0;
+        this._failed = {};
+        let argv = process.argv || {};
+        let namespace = qx.core.Environment.get("testnode.testNameSpace") || "qx.test";        
+        this.loader = new qx.dev.unit.TestLoaderBasic();
+        this.loader.setTestNamespace(namespace);
+        let clazzes = this.loader.getSuite().getTestClasses();
+        if (argv.class) {
+          let matcher = new RegExp(argv.class);
+          this.info("# running only test classes that match " + matcher);
+          clazzes = clazzes.filter(clazz => clazz.getName().match(matcher));
+        }
+
+        let pChain = new qx.Promise(resolve => resolve(true));
+        clazzes.forEach(
+          clazz => {
+            pChain = pChain.then(() =>
+              this.runAll(argv, clazz)
+                .then(() => {
+                  this.info(`# done testing ${clazz.getName()}.`);
+                })
+            );
+          }
+        );
+
+        return pChain.then(() => {
+          this.info(`1..${this._cnt}`);
+        });
+      },
+
+      runAll: function (argv, clazz) {
+        let that = this;
+        this.info(`# start testing ${clazz.getName()}.`);
+        let methods = clazz.getTestMethods();
+        if (argv.method) {
+          let matcher = new RegExp(argv.method);
+          this.info("# running only test methods that match " + matcher);
+          methods = methods.filter(method => method.getName().match(matcher));
+        }
+
+        return new qx.Promise(resolve => {
+          let testResult = new qx.dev.unit.TestResult();
+          let methodNameIndex = -1;
+          let next = () => {
+            methodNameIndex++;
+            if (methodNameIndex < methods.length) {
+              that.loader.runTests(
+                testResult,
+                clazz.getName(),
+                methods[methodNameIndex].getName()
+              );
+            } else {
+              resolve();
+            }
+          };
+          let showExceptions = arr => {
+            arr.forEach(item => {
+              if (item.test.getFullName) {
+                let test = item.test.getFullName();
+                that._failed[test] = true;
+                that._cnt++;
+                that._fail++;
+                let message = String(item.exception);
+                if (item.exception) {
+                  if (item.exception.message) {
+                    message = item.exception.message;
+                    this.info(`not ok ${that._cnt} - ${test} - ${message}`);
+                  } else {
+                    this.error("# " + item.exception);
+                  }
+                }
+              } else {
+                this.error("Unexpected Error - ", item);
+              }
+            });
+            setTimeout(next, 0);
+          };
+          testResult.addListener("startTest", evt => {
+            this.info("# start " + evt.getData().getFullName());
+          });
+          testResult.addListener("wait", evt => {
+            this.info("# wait " + evt.getData().getFullName());
+          });
+          testResult.addListener("endMeasurement", evt => {
+            this.info("# endMeasurement " + evt.getData()[0].test.getFullName());
+          });
+          testResult.addListener("endTest", evt => {
+            let test = evt.getData().getFullName();
+            if (!that._failed[test]) {
+              that._cnt++;
+              this.info(`ok ${that._cnt} - ` + test);
+            }
+            setTimeout(next, 0);
+          });
+          testResult.addListener("failure", evt => showExceptions(evt.getData()));
+          testResult.addListener("error", evt => showExceptions(evt.getData()));
+          testResult.addListener("skip", evt => {
+            that._cnt++;
+            let test = evt.getData()[0].test.getFullName();
+            that._failed[test] = true;
+            this.info(`ok ${that._cnt} - # SKIP ${test}`);
+          });
+          next();
+        });
+      },
+      _cnt: null,
+      _fail: null,
+      _failed: null
+    }
+  });
